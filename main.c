ccd4eac14a6c5a0fae5736e03aa49d6654e518ab
IamNomadSauce <garsteckphotography@gmail.com>
2024-04-06 21:32:29 -0400

    Device names sorted for Intel, create struct for GPU

--- main.c
+++ main.c
@@ -3,12 +3,15 @@
 #include <stdio.h>
 #include <CL/cl.h>
 
-void listIntelGPUs() {
+char* listIntelGPUs() {
     cl_uint platformCount;
     cl_platform_id *platforms;
     cl_uint deviceCount;
     cl_device_id *devices;
     char deviceName[128];
+    char* result = NULL;
+    size_t resultSize = 0;
+    size_t resultCapacity = 0;
 
     // Get the number of platforms
     clGetPlatformIDs(0, NULL, &platformCount);
@@ -28,16 +31,30 @@ void listIntelGPUs() {
         for (cl_uint j = 0; j < deviceCount; j++) {
             // Get the name of the GPU device
             clGetDeviceInfo(devices[j], CL_DEVICE_NAME, 128, deviceName, NULL);
-            printf("GPU Device %d: %s\n", j, deviceName);
+            
+            // Allocate or resize result buffer
+            size_t neededSize = resultSize + snprintf(NULL, 0, "%d %s\n", j, deviceName);
+            if (neededSize >= resultCapacity) {
+                resultCapacity = neededSize + 1; // +1 for null-terminator
+                result = realloc(result, resultCapacity);
+            }
+            
+            // Append device info to result
+            resultSize += sprintf(result + resultSize, "%d %s\n", j, deviceName);
         }
 
         free(devices);
     }
 
     free(platforms);
+    return result; // Caller is responsible for freeing this memory
 }
+
+#ifndef CGO
 
 int main() {
     listIntelGPUs();
     return 0;
 }
+#endif
+
--- main.go
+++ main.go
@@ -4,200 +4,246 @@ package main
 #cgo LDFLAGS: -lOpenCL
 #define CL_TARGET_OPENCL_VERSION 300
 #include <CL/cl.h>
+#cgo CFLAGS: -DCGO
+#include "main.c"
 */
 import "C"
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"strconv"
+	"regexp"
 	"strings"
-	"time"
+	"unsafe"
 	_ "unsafe"
-
-	"github.com/gorilla/websocket"
 	// "github.com/gorilla/websocket"
 	// _ "github.com/jgillich/go-opencl/cl"
 )
 
-const kernelSource = `
-__kernel void add(__global const float* a, __global const float* b, __global float* c) {
-    int gid = get_global_id(0);
-    c[gid] = a[gid] + b[gid];
+type GPU struct {
+	Vendor string   `json:vendor`
+	Name   []string `json:name`
 }
-`
+
+type GPU_Devices struct {
+	Devices []GPU `json:devices`
+}
+
+func listIntelGPUs() {
+	C.listIntelGPUs()
+}
+func main() {
+	fmt.Println("Getting GPU Devices")
+	resultC := C.listIntelGPUs()
+	defer C.free(unsafe.Pointer(resultC))
+	resultGo := C.GoString(resultC)
+	if strings.Contains(resultGo, "Intel") {
+		dev := FindAllStringsBetweenBrackets(resultGo)
+		// fmt.Println("Intel GPU", dev)
+		device := GPU{
+			Vendor: "Intel",
+			Name:   dev,
+		}
+		fmt.Println("Device", device)
+
+	}
+
+	// listIntelGPUs()
+	// http.HandleFunc("/ws", serveWs)
+	// fmt.Println("Starting Server on port:8069 ")
+	// http.ListenAndServe(":8069", nil)
+
+	// getDevices()
+}
+
+// FindAllStringsBetweenBrackets finds and returns all occurrences of strings between square brackets.
+func FindAllStringsBetweenBrackets(str string) []string {
+	re := regexp.MustCompile(`\[([^\[\]]+)\]`)
+	matches := re.FindAllStringSubmatch(str, -1)
+	var results []string
+	for _, match := range matches {
+		if len(match) > 1 {
+			results = append(results, match[1]) // Append the first capturing group.
+		}
+	}
+	return results
+}
+
+// const kernelSource = `
+// __kernel void add(__global const float* a, __global const float* b, __global float* c) {
+//     int gid = get_global_id(0);
+//     c[gid] = a[gid] + b[gid];
+// }
+// `
 
 // GPUData struct to hold all the GPU information
-type GPUData struct {
-	Temperature        float64 `json:"temperature"`
-	PowerConsumption   float64 `json:"powerConsumption"`
-	VramUsedPercentage float64 `json:"vramUsedPercentage"`
-	GPUUtilization     int     `json:"gpuUtilization"`
-	Sclk               int     `json:"sclk"`
-	Mclk               int     `json:"mclk"`
-}
+// type  AMD_GPUData struct {
+// 	Temperature        float64 `json:"temperature"`
+// 	PowerConsumption   float64 `json:"powerConsumption"`
+// 	VramUsedPercentage float64 `json:"vramUsedPercentage"`
+// 	GPUUtilization     int     `json:"gpuUtilization"`
+// 	Sclk               int     `json:"sclk"`
+// 	Mclk               int     `json:"mclk"`
+// }
 
-// TODO Add Device info/retrieval
+// var upgrader = websocket.Upgrader{
+// 	CheckOrigin: func(r *http.Request) bool {
+// 		return true // Allow connections from any origin
+// 	},
+// }
 
-var upgrader = websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true // Allow connections from any origin
-	},
-}
-
-func serveWs(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		fmt.Println("Error upgrading to WebSocket:", err)
-		return
-	}
-	defer conn.Close()
-	for {
-
-		// GPU Temperature
-		tempFilePath := "/sys/class/drm/card0/device/hwmon/hwmon3/temp1_input"
-		tempStr, err := ioutil.ReadFile(tempFilePath)
-		if err != nil {
-			fmt.Println("Error reading temperature:", err)
-			return
-		}
-
-		// The temperature is usually reported in millidegrees Celsius; convert to degrees
-		tempMilliDegrees := strings.TrimSpace(string(tempStr))
-		tempDegrees, err := strconv.Atoi(tempMilliDegrees)
-		if err != nil {
-			fmt.Println("Error converting temperature:", err)
-			return
-		}
-		// fmt.Printf("GPU Temperature: %.2f°C\n", float64(tempDegrees)/1000.0)
-
-		// Average Power Consumption
-		powerFilePath := "/sys/class/drm/card0/device/hwmon/hwmon3/power1_average"
-		powerStr, err := ioutil.ReadFile(powerFilePath)
-		if err != nil {
-			fmt.Println("Error reading power consumption:", err)
-			return
-		}
-
-		// The power consumption is usually reported in microwatts; convert to milliwatts
-		powerMicroWatts := strings.TrimSpace(string(powerStr))
-		powerMilliWatts, err := strconv.Atoi(powerMicroWatts)
-		if err != nil {
-			fmt.Println("Error converting power consumption:", err)
-			return
-		}
-
-		// //////////////
-		// sclk info
-		sclkinfo_fp := "/sys/class/drm/card0/device/hwmon/hwmon3/freq1_input" // Adjust as needed
-		sclkinfoBytes, err := ioutil.ReadFile(sclkinfo_fp)
-		if err != nil {
-			fmt.Println("Error reading clock info:", err)
-			return
-		}
-
-		// mclck info
-		mclkinfo_fp := "/sys/class/drm/card0/device/hwmon/hwmon3/freq2_input" // Adjust as needed
-		mclkinfoBytes, err := ioutil.ReadFile(mclkinfo_fp)
-		if err != nil {
-			fmt.Println("Error reading clock info:", err)
-			return
-		}
-
-		sclk_info := strings.TrimSpace(string(sclkinfoBytes))
-		sclk := 0
-		sclk, err = strconv.Atoi(sclk_info)
-		if err != nil {
-			// Handle the error
-			fmt.Println("Error converting string to int:", err)
-		} else {
-			// fmt.Println("Converted integer:", sclk)
-		}
-
-		mclk_info := strings.TrimSpace(string(mclkinfoBytes))
-		mclk := 0
-		mclk, err = strconv.Atoi(mclk_info)
-		if err != nil {
-			// Handle the error
-			fmt.Println("Error converting string to int:", err)
-		} else {
-			// fmt.Println("Converted integer:", mclk)
-		}
-
-		// /////////////////////////////////////////////////////
-
-		// VRam Usage
-		// Adjust these file paths as needed for your specific GPU and system
-		vramTotalFilePath := "/sys/class/drm/card0/device/mem_info_vram_total"
-		vramUsedFilePath := "/sys/class/drm/card0/device/mem_info_vram_used"
-
-		vramTotalBytes, err := ioutil.ReadFile(vramTotalFilePath)
-		if err != nil {
-			fmt.Println("Error reading total VRAM:", err)
-			return
-		}
-
-		vramUsedBytes, err := ioutil.ReadFile(vramUsedFilePath)
-		if err != nil {
-			fmt.Println("Error reading used VRAM:", err)
-			return
-		}
-
-		// Convert the read values from bytes to integers
-		vramTotal, err := strconv.Atoi(strings.TrimSpace(string(vramTotalBytes)))
-		if err != nil {
-			fmt.Println("Error converting total VRAM:", err)
-			return
-		}
-
-		vramUsed, err := strconv.Atoi(strings.TrimSpace(string(vramUsedBytes)))
-		if err != nil {
-			fmt.Println("Error converting used VRAM:", err)
-			return
-		}
-
-		// Calculate the percentage of VRAM used
-		vramUsedPercentage := (float64(vramUsed) / float64(vramTotal)) * 100
-
-		// fmt.Printf("GPU VRAM Usage: %.2f%%\n", vramUsedPercentage)
-
-		// GPU Utilization %
-		gpuUtilizationFilePath := "/sys/class/drm/card0/device/gpu_busy_percent" // Adjust as needed
-		gpuUtilizationStr, err := ioutil.ReadFile(gpuUtilizationFilePath)
-		if err != nil {
-			fmt.Println("Error reading GPU utilization:", err)
-			return
-		}
-
-		// Convert the utilization string to an integer
-		gpuUtilization, err := strconv.Atoi(strings.TrimSpace(string(gpuUtilizationStr)))
-		if err != nil {
-			fmt.Println("Error converting GPU utilization:", err)
-			return
-		}
-
-		// Populate the GPUData struct with your data
-		data := GPUData{
-			Temperature:        float64(tempDegrees) / 1000.0,        // Convert millidegrees to degrees
-			PowerConsumption:   float64(powerMilliWatts) / 1000000.0, // Convert microwatts to watts
-			VramUsedPercentage: vramUsedPercentage,
-			GPUUtilization:     gpuUtilization,
-			Sclk:               sclk,
-			Mclk:               mclk,
-		}
-
-		fmt.Printf("Temp: %f\nPower: %f\nSCLK: %d\nMCLK: %d\nGPU_prcnt: %d\nVRam: %f", data.Temperature, data.PowerConsumption, data.Sclk, data.Mclk, gpuUtilization, data.VramUsedPercentage)
-
-		// Send the data over the WebSocket connection
-		if err := conn.WriteJSON(data); err != nil {
-			fmt.Println("Error sending data:", err)
-			break
-		}
-		fmt.Println("\n")
-		time.Sleep(1 * time.Second)
-	}
-}
+// Uncomment to enable server again
+// func serveWs(w http.ResponseWriter, r *http.Request) {
+// 	conn, err := upgrader.Upgrade(w, r, nil)
+// 	if err != nil {
+// 		fmt.Println("Error upgrading to WebSocket:", err)
+// 		return
+// 	}
+// 	defer conn.Close()
+// 	for {
+//
+// 		// GPU Temperature
+// 		tempFilePath := "/sys/class/drm/card0/device/hwmon/hwmon3/temp1_input"
+// 		tempStr, err := ioutil.ReadFile(tempFilePath)
+// 		if err != nil {
+// 			fmt.Println("Error reading temperature:", err)
+// 			return
+// 		}
+//
+// 		// The temperature is usually reported in millidegrees Celsius; convert to degrees
+// 		tempMilliDegrees := strings.TrimSpace(string(tempStr))
+// 		tempDegrees, err := strconv.Atoi(tempMilliDegrees)
+// 		if err != nil {
+// 			fmt.Println("Error converting temperature:", err)
+// 			return
+// 		}
+// 		// fmt.Printf("GPU Temperature: %.2f°C\n", float64(tempDegrees)/1000.0)
+//
+// 		// Average Power Consumption
+// 		powerFilePath := "/sys/class/drm/card0/device/hwmon/hwmon3/power1_average"
+// 		powerStr, err := ioutil.ReadFile(powerFilePath)
+// 		if err != nil {
+// 			fmt.Println("Error reading power consumption:", err)
+// 			return
+// 		}
+//
+// 		// The power consumption is usually reported in microwatts; convert to milliwatts
+// 		powerMicroWatts := strings.TrimSpace(string(powerStr))
+// 		powerMilliWatts, err := strconv.Atoi(powerMicroWatts)
+// 		if err != nil {
+// 			fmt.Println("Error converting power consumption:", err)
+// 			return
+// 		}
+//
+// 		// //////////////
+// 		// sclk info
+// 		sclkinfo_fp := "/sys/class/drm/card0/device/hwmon/hwmon3/freq1_input" // Adjust as needed
+// 		sclkinfoBytes, err := ioutil.ReadFile(sclkinfo_fp)
+// 		if err != nil {
+// 			fmt.Println("Error reading clock info:", err)
+// 			return
+// 		}
+//
+// 		// mclck info
+// 		mclkinfo_fp := "/sys/class/drm/card0/device/hwmon/hwmon3/freq2_input" // Adjust as needed
+// 		mclkinfoBytes, err := ioutil.ReadFile(mclkinfo_fp)
+// 		if err != nil {
+// 			fmt.Println("Error reading clock info:", err)
+// 			return
+// 		}
+//
+// 		sclk_info := strings.TrimSpace(string(sclkinfoBytes))
+// 		sclk := 0
+// 		sclk, err = strconv.Atoi(sclk_info)
+// 		if err != nil {
+// 			// Handle the error
+// 			fmt.Println("Error converting string to int:", err)
+// 		} else {
+// 			// fmt.Println("Converted integer:", sclk)
+// 		}
+//
+// 		mclk_info := strings.TrimSpace(string(mclkinfoBytes))
+// 		mclk := 0
+// 		mclk, err = strconv.Atoi(mclk_info)
+// 		if err != nil {
+// 			// Handle the error
+// 			fmt.Println("Error converting string to int:", err)
+// 		} else {
+// 			// fmt.Println("Converted integer:", mclk)
+// 		}
+//
+// 		// /////////////////////////////////////////////////////
+//
+// 		// VRam Usage
+// 		// Adjust these file paths as needed for your specific GPU and system
+// 		vramTotalFilePath := "/sys/class/drm/card0/device/mem_info_vram_total"
+// 		vramUsedFilePath := "/sys/class/drm/card0/device/mem_info_vram_used"
+//
+// 		vramTotalBytes, err := ioutil.ReadFile(vramTotalFilePath)
+// 		if err != nil {
+// 			fmt.Println("Error reading total VRAM:", err)
+// 			return
+// 		}
+//
+// 		vramUsedBytes, err := ioutil.ReadFile(vramUsedFilePath)
+// 		if err != nil {
+// 			fmt.Println("Error reading used VRAM:", err)
+// 			return
+// 		}
+//
+// 		// Convert the read values from bytes to integers
+// 		vramTotal, err := strconv.Atoi(strings.TrimSpace(string(vramTotalBytes)))
+// 		if err != nil {
+// 			fmt.Println("Error converting total VRAM:", err)
+// 			return
+// 		}
+//
+// 		vramUsed, err := strconv.Atoi(strings.TrimSpace(string(vramUsedBytes)))
+// 		if err != nil {
+// 			fmt.Println("Error converting used VRAM:", err)
+// 			return
+// 		}
+//
+// 		// Calculate the percentage of VRAM used
+// 		vramUsedPercentage := (float64(vramUsed) / float64(vramTotal)) * 100
+//
+// 		// fmt.Printf("GPU VRAM Usage: %.2f%%\n", vramUsedPercentage)
+//
+// 		// GPU Utilization %
+// 		gpuUtilizationFilePath := "/sys/class/drm/card0/device/gpu_busy_percent" // Adjust as needed
+// 		gpuUtilizationStr, err := ioutil.ReadFile(gpuUtilizationFilePath)
+// 		if err != nil {
+// 			fmt.Println("Error reading GPU utilization:", err)
+// 			return
+// 		}
+//
+// 		// Convert the utilization string to an integer
+// 		gpuUtilization, err := strconv.Atoi(strings.TrimSpace(string(gpuUtilizationStr)))
+// 		if err != nil {
+// 			fmt.Println("Error converting GPU utilization:", err)
+// 			return
+// 		}
+//
+// 		// Populate the GPUData struct with your data
+// 		data := GPUData{
+// 			Temperature:        float64(tempDegrees) / 1000.0,        // Convert millidegrees to degrees
+// 			PowerConsumption:   float64(powerMilliWatts) / 1000000.0, // Convert microwatts to watts
+// 			VramUsedPercentage: vramUsedPercentage,
+// 			GPUUtilization:     gpuUtilization,
+// 			Sclk:               sclk,
+// 			Mclk:               mclk,
+// 		}
+//
+// 		fmt.Printf("Temp: %f\nPower: %f\nSCLK: %d\nMCLK: %d\nGPU_prcnt: %d\nVRam: %f", data.Temperature, data.PowerConsumption, data.Sclk, data.Mclk, gpuUtilization, data.VramUsedPercentage)
+//
+// 		// Send the data over the WebSocket connection
+// 		if err := conn.WriteJSON(data); err != nil {
+// 			fmt.Println("Error sending data:", err)
+// 			break
+// 		}
+// 		fmt.Println("\n")
+// 		time.Sleep(1 * time.Second)
+// 	}
+// }
 
 // func getDevices() {
 // 	var numPlatforms C.cl_uint
@@ -290,11 +336,3 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 // func main() {
 // 	getDevices()
 // }
-
-func main() {
-	http.HandleFunc("/ws", serveWs)
-	fmt.Println("Starting Server on port:8069 ")
-	http.ListenAndServe(":8069", nil)
-
-	// getDevices()
-}
